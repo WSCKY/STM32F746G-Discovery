ed3d3e2160efd130c96b0d5e23d21ebf2b3a679e
kyChu <hello.kychu@gmail.com>
2017-12-12 13:06:10 +0800

    enable camera interrupt handler

--- Projects/GC0329/src/gc0329_camera.c
+++ Projects/GC0329/src/gc0329_camera.c
@@ -372,6 +372,16 @@ void BSP_CAMERA_ColorEffectConfig(uint32_t Effect)
   }  
 }
 
+void BSP_CAMERA_IRQHandler(void)
+{
+	HAL_DCMI_IRQHandler(&hDcmiHandler);
+}
+
+void BSP_CAMERA_DMA_IRQHandler(void)
+{
+	HAL_DMA_IRQHandler(hDcmiHandler.DMA_Handle);
+}
+
 /**
   * @brief  Get the capture size in pixels unit.
   * @param  resolution: the current resolution.
